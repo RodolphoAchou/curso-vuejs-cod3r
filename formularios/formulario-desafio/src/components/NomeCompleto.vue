9888a437b6bc9a801a2313ba752aaffdaa03c726
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-08-14 18:27:19 -0300

    Conclusão dos itens 1 e 2 do desafio

---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="rotulo">
+        <label>Nome Completo:</label>
+        <div class="elemento">
+            <p>Rodolpho Achou A</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .rotulo {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin: 15px 0px;
+        font-size: 1.4rem;
+    }
+
+    .elemento {
+        display: flex;
+        align-self: stretch;
+        margin: 10px 0px;
+    }
+</style>
