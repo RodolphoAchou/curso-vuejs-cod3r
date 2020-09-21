cee5798845160a43dc598f4288448146c8d6bc37
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-09-21 15:33:19 -0300

    Criando as rotas do projeto

--- projeto-03-stock-trader/stock-trader/src/App.vue
+++ projeto-03-stock-trader/stock-trader/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-	<Header />
+	<v-app>
+		<v-content>
+			<v-container>
+				<router-view></router-view>
+			</v-container>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
-import Header from './components/Header'
+//import Header from './components/Header'
 export default {
-	components: {Header}
+	//components: {Header}
 }
 </script>
 
