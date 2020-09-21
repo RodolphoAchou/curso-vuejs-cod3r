5ca6cb39fcf737f2545eee5ff1952fbd1502407b
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-09-21 16:51:18 -0300

    Adicionando o Botão Comprar

--- projeto-03-stock-trader/stock-trader/src/components/Header.vue
+++ projeto-03-stock-trader/stock-trader/src/components/Header.vue
@@ -13,21 +13,24 @@
         <v-spacer></v-spacer>
 
         <v-toolbar-items>
-            <v-btn flat @click="endDay">Finalizar Dia</v-btn>
+            <!-- <v-btn flat @click="endDay">Finalizar Dia</v-btn> -->
+            <v-btn flat >Finalizar Dia</v-btn>
             <v-menu offset-y>
                 <v-btn flat slot="activator">Salvar & Carregar</v-btn>
                 <v-list>
-                    <v-list-tile @click="saveData">
+                    <v-list-tile >
+                    <!-- <v-list-tile @click="saveData"> -->
                         <v-list-tile-title>Salvar Dados</v-list-tile-title>
                     </v-list-tile>
-                    <v-list-tile @click="loadDataLocal">
+                    <v-list-tile >
+                    <!-- <v-list-tile @click="loadDataLocal"> -->
                         <v-list-tile-title>Carregar Dados</v-list-tile-title>
                     </v-list-tile>
                 </v-list>
             </v-menu>
             <v-layout align-center>
                 <span class="text-uppercase grey--text text--darken-2">
-                    Saldo: {{ funds | currency }}
+                    <!-- Saldo: {{ funds | currency }} -->
                 </span>
             </v-layout>
         </v-toolbar-items>
