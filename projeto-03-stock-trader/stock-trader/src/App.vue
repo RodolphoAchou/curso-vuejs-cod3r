8c9db3a61536e050af363dfcf738911e8e281298
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-09-21 15:46:16 -0300

    Criando o header do projeto

--- projeto-03-stock-trader/stock-trader/src/App.vue
+++ projeto-03-stock-trader/stock-trader/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<v-app>
+		<Header />
 		<v-content>
 			<v-container>
 				<router-view></router-view>
@@ -9,9 +10,9 @@
 </template>
 
 <script>
-//import Header from './components/Header'
+import Header from './components/Header'
 export default {
-	//components: {Header}
+	components: {Header}
 }
 </script>
 
--- projeto-03-stock-trader/stock-trader/src/components/Header.vue
+++ projeto-03-stock-trader/stock-trader/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-toolbar>
+    <v-toolbar app>
         <v-toolbar-title class="headline text-uppercase mr-4">
             <span>Stock</span>
             <span class="font-weight-light">Trader</span>
