8962926f7b26128c3443d3c9ec01f43c56b73da9
Rodolpho Achou <rodolphodealmeida@hotmail.com>
2020-09-22 09:21:07 -0300

    Finalizando Dia - Alterando Preço das Ações

--- projeto-03-stock-trader/stock-trader/src/components/Header.vue
+++ projeto-03-stock-trader/stock-trader/src/components/Header.vue
@@ -13,8 +13,7 @@
         <v-spacer></v-spacer>
 
         <v-toolbar-items>
-            <!-- <v-btn flat @click="endDay">Finalizar Dia</v-btn> -->
-            <v-btn flat >Finalizar Dia</v-btn>
+            <v-btn flat @click="endDay">Finalizar Dia</v-btn>
             <v-menu offset-y>
                 <v-btn flat slot="activator">Salvar & Carregar</v-btn>
                 <v-list>
@@ -38,11 +37,19 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
     computed: {
         funds () {
             return this.$store.getters.funds
         }
+    },
+    methods: {
+        ...mapActions(['randomizeStocks']),
+        endDay() {
+            this.randomizeStocks()
+        }    
     }
 }
 </script>
